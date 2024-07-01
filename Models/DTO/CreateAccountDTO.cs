0deb823233b6f62a13fa3fbed7b6eb21a02c560f
itzteeler <tylernguyen518@gmail.com>
2024-07-01 09:26:00 -0700

    Nullable - CreateAccountDTO / FIRSTNAME LASTNAME DOB

--- Models/DTO/CreateAccountDTO.cs
+++ Models/DTO/CreateAccountDTO.cs
@@ -10,9 +10,9 @@ namespace BackendRedo.Models.DTO
         public int ID { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string DOB { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? DOB { get; set; }
         public bool IsAdmin { get; set; }
         
     }
